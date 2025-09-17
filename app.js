5ed62c62cfbe89c39ee8d9e80cb3872234882403
taina-santos <santostainamd@gmail.com>
2025-09-16 23:30:39 -0500

    Alteração após o prompt de inserção de autenticação

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userController = require('./controller/userController');
 const transferController = require('./controller/transferController');
+const { authenticateToken } = require('./middleware/auth');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
@@ -12,9 +13,9 @@ app.post('/users/register', userController.register);
 app.post('/users/login', userController.login);
 app.get('/users', userController.list);
 
-// Transfer route
-app.post('/transfer', transferController.transfer);
-app.get('/transfer', transferController.listTransfers);
+// Transfer routes (protegidas por autenticação)
+app.post('/transfer', authenticateToken, transferController.transfer);
+app.get('/transfer', authenticateToken, transferController.listTransfers);
 
 // Swagger docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
