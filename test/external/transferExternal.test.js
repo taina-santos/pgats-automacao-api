099205cfd592091ab4b1780451fdc8907b6e27fb
taina-santos <santostainamd@gmail.com>
2025-09-18 23:44:32 -0500

    Adicionando json e teste http com retorno de sucesso

--- test/external/transferExternal.test.js
+++ test/external/transferExternal.test.js
@@ -1,14 +1,14 @@
 const request = require('supertest');
-const { expect } = require('chai');
+const { expect, use } = require('chai');
+const chaiExclude = require('chai-exclude');
+use(chaiExclude);
 
 const host = 'http://localhost:3000';
+var token;
 
 describe('External transfer via HTTP', () => {
   describe('POST /transfer', () => {
-    //---------------------------------------------
-    // TESTE VIA HTTP
-    //---------------------------------------------
-    it('Teste via http: Quando informo remetente e destinatário inexistente, o retorno será 400', async () => {
+    beforeEach(async () => {
       // 1) Caputar o token
       const login = await request(host)
         .post('/users/login')
@@ -16,12 +16,19 @@ describe('External transfer via HTTP', () => {
           username: 'taina',
           password: '123456'
         });
+      
+      token = login.body.token;
+    });
 
+    //---------------------------------------------
+    // TESTE VIA HTTP
+    //---------------------------------------------
+    it('Teste via http: Quando informo remetente e destinatário inexistente, o retorno será 400', async () => {
       // 2) Realizar a transferência
       // Para o uso do servidor rest, a gente modifica o request(app) para
       const resposta = await request(host)
         .post('/transfer')
-        .set('Authorization', `Bearer ${login.body.token}`)
+        .set('Authorization', `Bearer ${token}`)
         //.auth(login.body.token, {type: "bearer"})
         .send({
           from: "taina",
@@ -31,6 +38,21 @@ describe('External transfer via HTTP', () => {
       
       expect(resposta.status).to.equal(400);
       expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado')
+    });
+
+    it('Teste via http: Quando uso dados válidos, o retorno será 201', async () => {
+      const resposta = await request(host)
+        .post('/transfer')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          from: "taina",
+          to: "rafael",
+          amount: 100
+        });
+
+      const respostaEsperada = require('../fixture/responses/respostaTransferExternal201.json');
+      expect(resposta.status).to.equal(201);
+      expect(resposta.body).excluding('date').to.deep.equal(respostaEsperada);
     });
   });
 });
